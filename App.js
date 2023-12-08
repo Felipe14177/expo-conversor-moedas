8a86857ce3744198e894fdd9095ed5c1a691622a
Felipe14177 <141411365+Felipe14177@users.noreply.github.com>
2023-12-08 01:38:19 +0000

    first commit

--- App.js
+++ App.js
@@ -4,42 +4,40 @@ import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
 
 export default function App() {
-  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
-  const [moedaDestino, setMoedaDestino] = useState('USD')
-  const [valorEntrada, setVAlorEntrada] = useState('33.33')
-  const [resultado, setResultado] = useState('')
+  const [moedaOrigem, setMoedaOrigem] = useState('BRL');
+  const [moedaDestino, setMoedaDestino] = useState('USD');
+  const [valorEntrada, setValorEntrada] = useState('');
+  const [resultado, setResultado] = useState('');
 
   const handleConverter = async () => {
     let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`;
     try {
       let page = await fetch(URL);
       let json = await page.json();
-      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high)
-      let valor = parseFloat(valorEntrada)
-      setResultado((indice*valor).toFixed(2))
+      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high);
+      let valor = parseFloat(valorEntrada);
+      setResultado((indice * valor).toFixed(2));
     } catch (error) {
-      setResultado(`Erro: ${error.message}`)
+      setResultado(`Erro: ${error.message}`);
     }
-   }
+  };
 
   const handleLimpar = () => {
     setResultado('');
-    setVAlorEntrada('33.33333');
+    setValorEntrada('');
     setMoedaOrigem('BRL');
     setMoedaDestino('USD');
-   }
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Convesor de Moedas</Text>
-      <View>
+      <Text style={styles.title}>Conversor de Moedas</Text>
+      <View style={styles.pickerContainer}>
         <Text style={styles.tbMoeda}>Moeda 1</Text>
         <Picker
           style={styles.picker}
           selectedValue={moedaOrigem}
-          onValueChange={(itemValue, itemIndex) =>
-            setMoedaOrigem(itemValue)
-          }>
+          onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}>
           <Picker.Item label="Real Brasileiro" value="BRL" />
           <Picker.Item label="Dólar Americano" value="USD" />
           <Picker.Item label="Peso Argentino" value="ARS" />
@@ -48,14 +46,12 @@ export default function App() {
           <Picker.Item label="Bitcoin" value="BTC" />
         </Picker>
       </View>
-      <View>
+      <View style={styles.pickerContainer}>
         <Text style={styles.tbMoeda}>Moeda 2</Text>
         <Picker
           style={styles.picker}
           selectedValue={moedaDestino}
-          onValueChange={(itemValue, itemIndex) =>
-            setMoedaDestino(itemValue)
-          }>
+          onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}>
           <Picker.Item label="Real Brasileiro" value="BRL" />
           <Picker.Item label="Dólar Americano" value="USD" />
           <Picker.Item label="Peso Argentino" value="ARS" />
@@ -64,22 +60,24 @@ export default function App() {
           <Picker.Item label="Bitcoin" value="BTC" />
         </Picker>
       </View>
-      <View>
-        <Text style={styles.tbMoeda}>Valo para Conversão</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.tbMoeda}>Valor para Conversão</Text>
         <TextInput
           style={styles.input}
           value={valorEntrada}
-          onChangeText={setVAlorEntrada}
-          keyboardType='numeric'>
-        </TextInput>
+          onChangeText={setValorEntrada}
+          keyboardType="numeric"
+        />
       </View>
       <Pressable onPress={handleConverter} style={styles.button}>
-        <Text style={styles.title}>Conveter</Text>
+        <Text style={styles.buttonText}>Converter</Text>
       </Pressable>
       <Pressable onPress={handleLimpar} style={styles.button}>
-        <Text style={styles.title}>Limpar</Text>
+        <Text style={styles.buttonText}>Limpar</Text>
       </Pressable>
-      <View><Text style={styles.lbResultado}>{resultado}</Text></View>
+      <View>
+        <Text style={styles.lbResultado}>{resultado}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -88,39 +86,57 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#2c3e50', // Background color
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    color: '#fff'
+    color: '#ecf0f1', // Font color
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  pickerContainer: {
+    marginBottom: 15,
   },
   picker: {
-    color: '#fff',
+    color: '#ecf0f1', // Font color
     width: 200,
     height: 50,
-    backgroundColor: '#000'
+    backgroundColor: '#34495e', // Picker background color
+    borderRadius: 5,
+  },
+  inputContainer: {
+    marginBottom: 15,
   },
   input: {
-    color: '#fff',
+    color: '#ecf0f1', // Font color
     textAlign: 'right',
     height: 40,
-    width: 200
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#ecf0f1', // Border color
+    borderRadius: 5,
+    paddingHorizontal: 10,
   },
   tbMoeda: {
-    color: '#fff'
+    color: '#ecf0f1', // Font color
   },
   button: {
     width: 200,
     height: 40,
-    paddingBottom: 10,
-    backgroundColor: '#999',
+    backgroundColor: '#3498db', // Button color
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 5
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#ecf0f1', // Font color
   },
   lbResultado: {
-    color: '#fff'
-  }
+    color: '#ecf0f1', // Font color
+    fontSize: 18,
+    marginTop: 10,
+  },
 });
